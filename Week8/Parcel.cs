3bf5fae3ea08d480c2a6e64879dcfe1900a19453
_KWAZARD_ <144619821+KWAZARD@users.noreply.github.com>
2026-03-24 18:31:49 +0200

    1Step

---
+++ Week8/Parcel.cs
@@ -0,0 +1,21 @@
+﻿namespace Week8;
+
+public class Parcel : DeliveryItem
+{
+    public string Dimension { get; set; }
+    public Parcel(string trackingNumber, double weight, string dimension) : base(trackingNumber, weight)
+    {
+        Dimension = dimension;
+    }
+
+    public override double CalculateCost()
+    {
+        return 50 + Weight * 25;
+    }
+
+    public override void PrintInfo()
+    {
+        base.PrintInfo();
+        Console.WriteLine($"Dimension: {Dimension}");
+    }
+}
