64c57b13407bca5c8ad6f4b59baac90ba0abd2d9
_KWAZARD_ <144619821+KWAZARD@users.noreply.github.com>
2026-03-24 18:37:42 +0200

    2Step

---
+++ Week8/CargoContainer.cs
@@ -0,0 +1,23 @@
+﻿namespace Week8;
+
+public class CargoContainer<T> where T: DeliveryItem
+{
+    private List<T> saveObject = new List<T>();
+
+    public void AddItem(T value)
+    {
+        saveObject.Add(value);
+    }
+
+    public double GetTotalCost()
+    {
+        double sum = 0;
+        foreach (var saveObj in saveObject)
+        {
+            sum += saveObj.CalculateCost();
+        }
+
+        return sum;
+    }
+    
+}
